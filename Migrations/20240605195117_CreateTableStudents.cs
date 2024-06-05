8459274dcb2f49f4eb67bde818bd2844fb38768c
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-06-05 14:53:28 -0500

    feat: The table "students" was created in the database

---
+++ Migrations/20240605195117_CreateTableStudents.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api_school.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTableStudents : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "students",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    full_name = table.Column<string>(type: "varchar(125)", maxLength: 125, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    birthdate = table.Column<DateOnly>(type: "date", nullable: false),
+                    address = table.Column<string>(type: "varchar(125)", maxLength: 125, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    email = table.Column<string>(type: "varchar(125)", maxLength: 125, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_students", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "students");
+        }
+    }
+}
