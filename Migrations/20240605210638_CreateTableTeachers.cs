af2284c1157512680120fe116d4f79633d9d9736
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-06-05 16:07:19 -0500

    feat: The table "teachers" was created in the database

---
+++ Migrations/20240605210638_CreateTableTeachers.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api_school.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTableTeachers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "teachers",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    full_name = table.Column<string>(type: "varchar(125)", maxLength: 125, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    specialty = table.Column<string>(type: "varchar(125)", maxLength: 125, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    phone = table.Column<string>(type: "varchar(25)", maxLength: 25, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    email = table.Column<string>(type: "varchar(125)", maxLength: 125, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    years_experience = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_teachers", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "teachers");
+        }
+    }
+}
