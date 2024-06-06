7d3a49c8fb1d995fd966196fead94487c72803bf
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-06-05 19:03:07 -0500

    feat: the enpoint for get all course for teacherId was created

--- Controllers/Teachers/TeachersController.cs
+++ Controllers/Teachers/TeachersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api_school.Models;
+using api_school.Services.Courses;
 using api_school.Services.Teachers;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,10 +14,12 @@ namespace api_school.Controllers.Teachers
     public class TeachersController : ControllerBase
     {
         private readonly ITeacherRepository _teacherRepository;
+        private readonly ICourseRepository _courseRepository;
 
-        public TeachersController(ITeacherRepository teacherRepository)
+        public TeachersController(ITeacherRepository teacherRepository, ICourseRepository courseRepository)
         {
             _teacherRepository = teacherRepository;
+            _courseRepository = courseRepository;
         }
 
         [HttpGet]
@@ -36,6 +39,18 @@ namespace api_school.Controllers.Teachers
             }
 
             return Ok(teacher);
+        }
+
+        [HttpGet("{id}/courses")]
+        public async Task<ActionResult<IEnumerable<Course>>> GetCoursesByTeacherId(int id)
+        {
+            var allCourses = await _courseRepository.GetAll();
+            var teacherCourses = allCourses.Where(c => c.TeacherId == id).ToList();
+            if (!teacherCourses.Any())
+            {
+                return NotFound(new { message = $"No courses found for teacher with ID {id}." });
+            }
+            return Ok(teacherCourses);
         }
     }
 }
